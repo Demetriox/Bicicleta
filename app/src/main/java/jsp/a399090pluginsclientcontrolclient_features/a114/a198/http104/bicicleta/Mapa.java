e2f7cdcfcf0a811f2f3da9cedb0d1ac5671c83e1
Demetriox <jesusdemetrio04@gmail.com>
2017-09-09 15:14:51 -0700

    Subida de archivos
    proyecto android

---
+++ app/src/main/java/jsp/a399090pluginsclientcontrolclient_features/a114/a198/http104/bicicleta/Mapa.java
@@ -0,0 +1,13 @@
+package jsp.a399090pluginsclientcontrolclient_features.a114.a198.http104.bicicleta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mapa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa);
+    }
+}
---
+++ app/src/main/java/jsp/a399090pluginsclientcontrolclient_features/a114/a198/http104/bicicleta/seguros.java
@@ -0,0 +1,29 @@
+package jsp.a399090pluginsclientcontrolclient_features.a114.a198.http104.bicicleta;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class seguros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seguros);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
